4b6d3d7d650688a716fab1642ea4a5f25a111240
Josh Melgar <josh.melgar@gmail.com>
2025-07-13 18:38:17 -0400

    cleanup 4

--- src/astronaut-helmet/AstronautHelmet.tsx
+++ src/astronaut-helmet/AstronautHelmet.tsx
@@ -6,7 +6,7 @@ import { Group } from 'three';
 const AstronautHelmet = () => {
   const groupRef = useRef<Group>(null);
   const gltf = useLoader(GLTFLoader, '/space_helmet.glb');
-  
+
   return (
     <group ref={groupRef} position={[0, 0, 0]} rotation={[0, Math.PI, 0]} scale={[3, 3, 3]}>
       <primitive object={gltf.scene} />

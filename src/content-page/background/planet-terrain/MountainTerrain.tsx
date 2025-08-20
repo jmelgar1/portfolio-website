5f1ce0a8770a7e84fae8ab2bb1e2b1c0035d6db2
Josh Melgar <josh.melgar@gmail.com>
2025-08-20 17:21:07 -0400

    organization

--- src/content-page/background/planet-terrain/MountainTerrain.tsx
+++ src/content-page/background/planet-terrain/MountainTerrain.tsx
@@ -1,6 +1,6 @@
 import { useState, useRef, useEffect } from 'react';
 import * as THREE from 'three';
-import { useTerrainWorker } from '../../../workers/useTerrainWorker';
+import { useTerrainWorker } from '../../workers/useTerrainWorker';
 
 interface MountainTerrainProps {
   position?: [number, number, number];
